4e65795634488a7758d4f780081d6163c245673d
Aniket <aniketkiri4444@gmail.com>
2022-03-08 09:09:43 +0530

    edited

---
+++ students.py
@@ -0,0 +1,24 @@
+''''
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*#
+        Name :- Aniket
+        Sem :- 4th
+        Subjects:- PPDS.
+        project :- Lab Task-5
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*#
+'''
+print("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+student = {}
+
+i = int(input("Enter the total number of students :"))
+
+for i in range(i):
+    student['Roll.no.'] = int(input("Enter Roll.No of student :"))
+
+    student['Name'] = input("Enter the name of student:")
+
+    student['Marks'] = int(input("Enter the marks obtained by the student:"))
+
+    print(student)
+
+
+
